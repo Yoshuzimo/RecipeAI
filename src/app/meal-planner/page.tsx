00406b996b100990787ed95eb6969852516bb7e8
Joshua Johnson <psppay@gmail.com>
2025-08-17 19:17:13 +0000

    on the meal Planner page, remove the title on the top, we don't need tha

--- src/app/meal-planner/page.tsx
+++ src/app/meal-planner/page.tsx
@@ -8,9 +8,6 @@ export default async function MealPlannerPage() {
   return (
     <MainLayout>
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h1 className="text-3xl font-bold tracking-tight">Meal Planner</h1>
-        </div>
         <MealPlanner initialInventory={inventory} />
       </div>
     </MainLayout>
