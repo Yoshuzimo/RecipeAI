f11de9bddfa5eec08a59f32c62abaf57d8978fd3
Joshua Johnson <psppay@gmail.com>
2025-08-17 18:14:12 +0000

    Get rid of the Shopping List Title. we don't need that

--- src/app/shopping-list/page.tsx
+++ src/app/shopping-list/page.tsx
@@ -9,9 +9,6 @@ export default async function ShoppingListPage() {
   return (
     <MainLayout>
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h1 className="text-3xl font-bold tracking-tight">Shopping List</h1>
-        </div>
         <ShoppingList inventory={inventory} personalDetails={personalDetails} />
       </div>
     </MainLayout>
