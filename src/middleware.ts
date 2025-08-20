ac01ef2787a9232d5eed935daea18b100d431c68
Joshua Johnson <psppay@gmail.com>
2025-08-20 23:24:21 +0000

    OOPS, misspselled the fix, lol

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export const runtime = 'edge'; // 👈 Explicitly declare Edge runtime
+export const runtime = 'experimental-edge'; // 👈 Explicitly declare Edge runtime
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
