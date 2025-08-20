755ef959f49d1dab1decbdafed02b6e880a39d50
Joshua Johnson <psppay@gmail.com>
2025-08-20 22:36:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  serverExternalPackages: ['firebase-admin'],
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin'],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
